e6dcf9c117c879996470393fff75631eae98ce22
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2025-07-17 01:48:35 -0300

    chore: arrumando pro deploy

--- src/lib/caseStudies.ts
+++ src/lib/caseStudies.ts
@@ -15,4 +15,100 @@ export const caseStudies = [
     github: "https://github.com/gabriellefoliv/hidra",
     demo: "",
   },
+  {
+    slug: "sigcol",
+    title: "Plataforma Ambiental",
+    image: "",
+    techStack: ["Next.js", "Tailwind", "PostgreSQL", "Prisma"],
+    description:
+      "",
+    problem:
+      "",
+    solution:
+      "",
+    impact:
+      "",
+    github: "https://github.com/gabriellefoliv/hidra",
+    demo: "",
+  },
+  {
+    slug: "thamiris-advocacia",
+    title: "Plataforma Ambiental",
+    image: "",
+    techStack: ["Next.js", "Tailwind", "PostgreSQL", "Prisma"],
+    description:
+      "",
+    problem:
+      "",
+    solution:
+      "",
+    impact:
+      "",
+    github: "https://github.com/gabriellefoliv/hidra",
+    demo: "",
+  },
+  {
+    slug: "instituto-transformacao",
+    title: "Plataforma Ambiental",
+    image: "",
+    techStack: ["Next.js", "Tailwind", "PostgreSQL", "Prisma"],
+    description:
+      "",
+    problem:
+      "",
+    solution:
+      "",
+    impact:
+      "",
+    github: "https://github.com/gabriellefoliv/hidra",
+    demo: "",
+  },
+  {
+    slug: "isoas",
+    title: "Plataforma Ambiental",
+    image: "",
+    techStack: ["Next.js", "Tailwind", "PostgreSQL", "Prisma"],
+    description:
+      "",
+    problem:
+      "",
+    solution:
+      "",
+    impact:
+      "",
+    github: "https://github.com/gabriellefoliv/hidra",
+    demo: "",
+  },
+  {
+    slug: "astra",
+    title: "Plataforma Ambiental",
+    image: "",
+    techStack: ["Next.js", "Tailwind", "PostgreSQL", "Prisma"],
+    description:
+      "",
+    problem:
+      "",
+    solution:
+      "",
+    impact:
+      "",
+    github: "https://github.com/gabriellefoliv/hidra",
+    demo: "",
+  },
+  {
+    slug: "rotas",
+    title: "Plataforma Ambiental",
+    image: "",
+    techStack: ["Next.js", "Tailwind", "PostgreSQL", "Prisma"],
+    description:
+      "",
+    problem:
+      "",
+    solution:
+      "",
+    impact:
+      "",
+    github: "https://github.com/gabriellefoliv/hidra",
+    demo: "",
+  },
 ];
